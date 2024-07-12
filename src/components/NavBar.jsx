94c26c325be61a7d7362f0e86f69f5bd8c01b1ec
Artur Ferreira <ferreira.artur.rj@gmail.com>
2024-07-12 01:31:12 -0300

    busca

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,7 +6,7 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import InputBase from '@mui/material/InputBase';
-import { Button, CssBaseline, Typography } from '@mui/material';
+import { Button, CssBaseline, Typography,Autocomplete,TextField } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
 import Stack from '@mui/material/Stack';
 import MenuItem from '@mui/material/MenuItem';
@@ -15,11 +15,19 @@ import Badge from '@mui/material/Badge';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { Outlet, Link } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
 import PopoverCart from '../components/PopoverCart';
 
 
 
 import { CartContext } from "../contextAPI/CartContext";
+
+const options = [
+  { label: 'DevOps', value: '/courses/DevOps/#top' },
+  { label: 'Linux', value: '/courses/Linux/#top' },
+  { label: 'Cloud', value: '/courses/Cloud/#top' },
+  { label: 'Segurança', value: '/courses/Segurança/#top' },
+]; // Lista de opções com rótulos e valores de rota
 
 const theme = createTheme({
   typography: {
@@ -90,6 +98,7 @@ const Search = styled('div')(({ theme }) => ({
     },
   }));
 
+
   
 
   
@@ -102,12 +111,6 @@ function NavBar() {
 
   const [anchorEl, setAnchorEl] = React.useState(null);
 
-  const [openModal3, setOpenModal3] = React.useState(null);
-
-  const handleClick3 = (event) => {
-    setOpenModal3(event.currentTarget);
-  };
-
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
   };
@@ -116,20 +119,20 @@ function NavBar() {
     setAnchorEl(null);
   };
 
-  const [searchTerm, setSearchTerm] = useState('');
 
-  const handleChange = (event) => {
-    console.log (event);
-    const value = event.target.value;
-    console.log(value);
-    setSearchTerm(value);
-    //onChange(value);
+  const navigate = useNavigate();
+  const [value, setValue] = useState(null);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+    if (newValue) {
+      navigate(newValue.value); // Redireciona para a rota correspondente ao valor selecionado
+    }
   };
 
 
-  function isEmpty(carrinho){
-    return carrinho.length === 0;
-  }
+
+
 
   
   
@@ -158,20 +161,76 @@ function NavBar() {
                     </Box>
                   </Stack>
 
-                  <Search >
-                    <SearchIconWrapper>
-                      <SearchIcon />
-                    </SearchIconWrapper>
+{/* busca */}
+
+                  
+                   
+                  <Stack spacing={2} sx={{ width: 400 }}>
+                   
+                    
+                    <Autocomplete
+                      freeSolo
+                      id="free-solo-2-demo"
+                      disableClearable
+                      value={value}
+                      onChange={handleChange}
+                      options={options}
+                      getOptionLabel={(option) => option.label} // Função para obter o rótulo da opção
+                      noOptionsText="Nenhuma opção disponível" 
+                      renderInput={(params) => <TextField {...params} label=" O que gostaria de aprender?"
+                      
+                      InputLabelProps={{
+                        style: { textAlign: 'center', color: 'inherit' }, // Estilo para a cor do rótulo
+                      }}
+                      
+                      />}
+                      sx={{
+                        '& .MuiOutlinedInput-root': {
+                          border: 'none', // Remove a borda
+                          color: 'inherit',
+                          backgroundColor: alpha(theme.palette.common.white, 0.15),
+                         
+                          borderRadius: theme.shape.borderRadius,
+                          '&:hover': {
+                            border: 'none', // Remove a borda no hover também
+                            color: 'inherit',
+                            backgroundColor: alpha(theme.palette.common.white, 0.15),
+                            
+                            borderRadius: theme.shape.borderRadius,
+                          },
+                          '&.Mui-focused': {
+                            border: 'none', // Remove a borda quando o campo está focado
+                            color: 'inherit',
+                            backgroundColor: alpha(theme.palette.common.white, 0.15),
+                            
+                            borderRadius: theme.shape.borderRadius,
+                          },
+                        },
+                        '& .MuiOutlinedInput-notchedOutline': {
+                          border: 'none', // Remove a borda da variante outlined, se necessário
+                        },
+                        width: '400px',
+                        
+                        
+                        
+                      }}
+                      />
+                     
+                  </Stack>
+                   
+
+{/*
                     <StyledInputBase
                       placeholder="O que gostaria de aprender?"
                       inputProps={{ 'aria-label': 'search' }}
-                      onChange={handleChange}
+                      //onChange={handleChange}
                       sx={{
                         width: '400px'
                       }}
                     />
-
-                  </Search>
+                    */}
+                  
+{/* fim busca */}
 
                   <Box sx={{ flexGrow: 1 }} />
                   <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
